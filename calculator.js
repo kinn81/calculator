6061b45e679dd1d1d3f578d72500cf5e188ea0a0
Keir Innes <keir.innes@gmail.com>
2021-09-14 13:38:01 +1200

    Basic gui and JS logic. Not connected yet though

---
+++ calculator.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+function operate(num1, operator, num2) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    }
+}
+
+const calculatorDiv = document.querySelector('#calculator');
+
